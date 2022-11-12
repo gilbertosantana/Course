0234f66ee69e35075063647de08da51baa220984
gilbertosantana <gilbertosantoss307@gmail.com>
2022-11-12 20:27:37 -0300

    Studying files

---
+++ Archives/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+string sourcePath = @"C:\Users\Jessica\Downloads\file1.txt";
+string targetPath = @"C:\Users\Jessica\Downloads\file2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    fileInfo.CopyTo(targetPath);
+    string[] lines = File.ReadAllLines(sourcePath);
+
+    foreach(string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
---
+++ Files2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string path = @"C:\Users\Jessica\Downloads\file1.txt";
+
+StreamReader ?streamReader = null;
+
+try
+{
+	streamReader = File.OpenText(path);
+	while (!streamReader.EndOfStream)
+	{
+        string line = streamReader.ReadLine()!;
+        Console.WriteLine(line);
+    }
+}
+catch (IOException e)
+{
+	Console.WriteLine("An error occurred");
+	Console.WriteLine(e.Message);
+}
+finally 
+{ 
+	if(streamReader != null) streamReader.Close();
+}
