3204790f4cac10f2a69f685c7d63e5d1e2f03181
gilbertosantana <gilbertosantoss307@gmail.com>
2022-11-17 12:02:22 -0300

    Using gerenics

---
+++ Generics/PrintService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count++] = value;
+        }
+
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values.GetValue(i) + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values.GetValue(_count-1) + "]\n");
+            }
+        }
+
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using Generics;
+
+PrintService<int> ps = new PrintService<int>();
+
+Console.WriteLine("How many values?");
+
+int quantValues = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < quantValues; i++)
+{
+    int value = int.Parse(Console.ReadLine()!);
+    ps.AddValue(value);
+}
+ps.Print();
+Console.WriteLine("First: " + ps.First());
