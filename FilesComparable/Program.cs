0f49cc003e65c2e2ab8bcb9feb2288b510a5128f
gilbertosantana <gilbertosantoss307@gmail.com>
2022-11-18 13:54:44 -0300

    using IComparable

---
+++ FilesComparable/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using FilesComparable.Entities;
+
+string path = @"C:\temp\in.csv";
+
+try
+{
+    using(StreamReader sr = File.OpenText(path))
+    {
+        List<Employee> list = new List<Employee>();
+
+        while (!sr.EndOfStream)
+        {
+            list.Add(new Employee(sr.ReadLine()!));
+        }
+        list.Sort();
+
+        foreach (Employee str in list)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
